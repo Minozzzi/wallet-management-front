09e09ada1b9359d87f58634ae9f33ce95175e82f
Guilherme Minozzi <minozzi60@gmail.com>
2022-11-03 21:27:41 -0300

    feat(data/protocols): add base http client

---
+++ src/data/protocols/http/httpClient.ts
@@ -0,0 +1,27 @@
+export type HttpMethod = 'get' | 'post' | 'delete' | 'patch'
+
+export enum HttpStatusCode {
+	ok = 200,
+	noContent = 204,
+	badRequest = 400,
+	unauthorized = 401,
+	forbidden = 403,
+	notFound = 404,
+	serverError = 500
+}
+
+export type HttpRequest = {
+	url: string
+	method: HttpMethod
+	body?: unknown
+	headers?: unknown
+}
+
+export type HttpResponse<T = unknown> = {
+	statusCode: HttpStatusCode
+	body?: T
+}
+
+export interface HttpClient<T = unknown> {
+	request: (data: HttpRequest) => Promise<HttpResponse<T>>
+}
