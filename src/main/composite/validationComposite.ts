0228072c357ee6621450d133d8446f7240857dfc
Guilherme Minozzi <minozzi60@gmail.com>
2022-11-06 14:12:34 -0300

    feat(main): add validation composite

---
+++ src/main/composite/validationComposite.ts
@@ -0,0 +1,26 @@
+import { Validation } from '@/presentation/protocols'
+import { FieldValidation } from '@/validation/protocols'
+
+export class ValidationComposite implements Validation {
+	private constructor(private readonly validators: FieldValidation[]) {}
+
+	static build(validators: FieldValidation[]): ValidationComposite {
+		return new ValidationComposite(validators)
+	}
+
+	validate(fieldName: string, input: object): string | null {
+		const validators = this.validators.filter(
+			validator => validator.field === fieldName
+		)
+
+		for (const validator of validators) {
+			const error = validator.validate(input)
+
+			if (error) {
+				return error.message
+			}
+		}
+
+		return null
+	}
+}
