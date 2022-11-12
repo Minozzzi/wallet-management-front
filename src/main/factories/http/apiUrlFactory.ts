a5937bb83d5130d19aa36e7c151f2914f193d25c
Guilherme Minozzi <minozzi60@gmail.com>
2022-11-12 15:19:20 -0300

    refactor: remove unnecessary url factory

--- src/main/factories/http/apiUrlFactory.ts
+++
@@ -1,2 +0,0 @@
-export const makeApiUrl = (path: string): string =>
-	`${process.env.VUE_APP_API_URL}/${path.replace(/^\//, '')}`
--- src/main/factories/http/index.ts
+++ src/main/factories/http/index.ts
@@ -1,2 +1,1 @@
-export * from './apiUrlFactory'
 export * from './axiosHttpClientFactory'
