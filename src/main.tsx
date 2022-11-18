cf5e4f02968b6045dae50da5f565ae9c9b8d4705
Guilherme Minozzi <minozzi60@gmail.com>
2022-11-17 21:19:38 -0300

    feat: add react hot toast

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import ReactDOM from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 
-import { GlobalStyles } from './styles'
+import { GlobalStyles, sg } from './styles'
 import { Router } from '@/main/routes'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<GlobalStyles />
 		<Router />
+		<Toaster
+			position='top-right'
+			gutter={Number(sg.spacing.xxlarge.replace('px', ''))}
+		/>
 	</React.StrictMode>
 )
