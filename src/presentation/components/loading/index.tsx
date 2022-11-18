b5669566ae59ee1dcb95b1494653539793e7efbf
Guilherme Minozzi <minozzi60@gmail.com>
2022-11-17 21:20:58 -0300

    feat: add delay on loading component

--- src/presentation/components/loading/index.tsx
+++ src/presentation/components/loading/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import * as S from './styles'
 
@@ -7,8 +7,29 @@ type LoadingProps = {
 	 * It is the Loading size
 	 */
 	size?: number
+
+	/**
+	 * It is the delay to show the Loading
+	 */
+	delay?: number
 }
 
-export const Loading: React.FC<LoadingProps> = ({ size = 32 }) => {
-	return <S.Loader size={size} />
+export const Loading: React.FC<LoadingProps> = ({ size = 32, delay = 0 }) => {
+	const [show, setShow] = useState(false)
+
+	useEffect(() => {
+		let timeout: ReturnType<typeof setTimeout>
+
+		if (delay > 0) timeout = setTimeout(() => setShow(true), delay)
+
+		return () => {
+			clearTimeout(timeout)
+		}
+	}, [delay])
+
+	if (delay === 0) return <S.Loader size={size} />
+
+	if (show) return <S.Loader size={size} />
+
+	return null
 }
